7f1adef297b44428a422b581f8c517a960db4350
Brian Vanegas <brianvanegas@viajesaeromar.com>
2020-09-16 12:24:33 -0500

    [PX-2] Menu hamburger animation

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,14 +6,22 @@ const Header = () => {
 
   return (
     <>
-      <button
-        className="float-button-menu-toggle"
+      <svg
+        className={'ham ham4 hamRotate' + (mobileNav ? ' active' : '')}
+        viewBox="0 0 100 100"
+        width="50"
         onClick={() => setMobileNav(!mobileNav)}
       >
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </button>
+        <path
+          className="line top"
+          d="m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20"
+        />
+        <path className="line middle" d="m 70,50 h -40" />
+        <path
+          className="line bottom"
+          d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20"
+        />
+      </svg>
       <div className="nav-wrapper">
         <div className="grad-bar"></div>
         <nav className="navbar">
