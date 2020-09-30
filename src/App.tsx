39af40d5c8f13bf9a4c288bbc122250b18ca9996
Brian Vanegas <brianvanegas@viajesaeromar.com>
2020-09-29 19:28:52 -0500

    [PX-4] Crud categories

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import './App.scss'
 import Header from './components/layout/Header'
+import CategoriesPage from './pages/CategoriesPage'
 import Home from './pages/Home'
 import ProductPage from './pages/ProductPage'
 
@@ -12,6 +13,7 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/products" component={ProductPage} />
+          <Route exact path="/categories" component={CategoriesPage} />
           <Route path="/" component={Home} />
         </Switch>
       </div>
